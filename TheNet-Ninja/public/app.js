0f5ae1496bc1662a69476b30abcb51ed7ce3c1bd
David Ohis <davidi.ohis25@gmail.com>
2022-09-13 21:45:50 +0100

    new project

--- TheNet-Ninja/public/app.js
+++ TheNet-Ninja/public/app.js
@@ -1,1 +1,12 @@
 "use strict";
+// Form
+const form = document.querySelector(".new-item-form");
+// Inputs
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#toform");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.value);
+});
