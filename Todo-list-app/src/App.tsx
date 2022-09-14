e4c67f1147399d2514f585703b758ea54878f3c5
David Ohis <davidi.ohis25@gmail.com>
2022-09-14 20:26:19 +0100

    building todo

---
+++ Todo-list-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div className="App">Hello world</div>
+    </React.Fragment>
+  );
+};
+export default App;
